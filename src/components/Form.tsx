17a4c19a06dd2a402d25f9d3fdf61212446119d6
Nivs <nivsoficial@gmail.com>
2023-01-07 10:13:30 -0300

    feat: atts about forms

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,12 +9,21 @@ import {
     Heading,
     Button,
   } from '@chakra-ui/react'
+import React, { SetStateAction, useState } from 'react'
 
-  interface formProp {
-    title: string,
-    button: string,
-  }
-  const Form = (props: formProp) => {
+interface formProp {
+  title: string,
+  button: string,
+}
+
+const Form = (props: formProp) => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const handleSubmit: any = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+      console.log(email, 'email')
+      console.log(password, 'event')
+    }
     return (
         <Flex width="full" align="center" justifyContent="center">
         <Box p={2}>
@@ -22,14 +31,14 @@ import {
             <Heading>{props.title}</Heading>
           </Box>
           <Box my={4} textAlign="left">
-            <form>
+            <form onSubmit={handleSubmit}>
               <FormControl>
                 <FormLabel>Email</FormLabel>
-                <Input type="email" placeholder="test@test.com" />
+                <Input onChange={e => setEmail(String(e.target.value))} name="email" type="email" placeholder="test@test.com" />
               </FormControl>
               <FormControl mt={6}>
                 <FormLabel>Password</FormLabel>
-                <Input type="password" placeholder="*******" />
+                <Input onChange={e => setPassword(String(e.target.value))} name="password" type="password" placeholder="*******" />
               </FormControl>
               <Button width="full" mt={4} type="submit">
                 { props.button }
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,5 +2,5 @@ import Public from "./Public";
 import Logged from "./Logged";
 
 const auth = true;
-const Routes = auth ? Logged : Public
+const Routes = auth ? Public.concat(Logged) : Public
 export default Routes;
